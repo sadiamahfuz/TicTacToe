bdf64c23bc9d943ea83e01a7528e3b8c500a3808
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2018-03-25 22:19:16 +0100

    allow buttons clicks and display X on click

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "", 
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick = {() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
